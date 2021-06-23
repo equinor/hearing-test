9154432f91ae7e4be231190f1cf3f45a9da9a885
Tormod Aase <toraas@equinor.com>
2021-06-23 04:52:51 -0700

    Equinor font

--- app/components/common/atoms/Typography.tsx
+++ app/components/common/atoms/Typography.tsx
@@ -7,23 +7,25 @@ import { EQUINOR_GREEN } from '../../../stylesheets/colors';
  * I have therefore only made 3 variants 
  **/ 
 const Typography = (props:{variant:'h1' | 'h2' | 'p', children:any, style?:StyleSheet}) => {
-    let color, size, weight;
+    let color, size, weight,fontFamily;
     if (props.variant.startsWith('h')) {
         color = EQUINOR_GREEN
+        fontFamily= "Equinor-Regular";
         if (props.variant === 'h1') {
             size=28
-            weight='300'
+            weight='400'
         } else {
             size=18;
-            weight='400'
+            weight='500'
         }
     } else {
+        fontFamily="Equinor-Light"
         color = "#000000";
         size=18;
-        weight='300';
+        weight='400';
     }
 
-    return <Text style={{fontSize:size, fontWeight:weight, color, ...props.style}}>{props.children}</Text>
+    return <Text style={{fontSize:size, fontWeight:weight, color, fontFamily, ...props.style}}>{props.children}</Text>
 }
 
 export default Typography;
