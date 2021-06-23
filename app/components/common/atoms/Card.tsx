0ee79bb9cfe10ad7303e9e71ec88ceff4e4c1615
Tormod Aase <toraas@equinor.com>
2021-06-23 08:24:34 -0700

    Card component

---
+++ app/components/common/atoms/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+const Card = (props:{children?:any, onPress?:CallableFunction, style?:any}) => {
+    return <TouchableOpacity onPress={props.onPress} disabled={!props.onPress}><View style={{backgroundColor:'white', borderRadius:4, padding: 18, margin:4,
+        shadowColor: '#000',
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,  
+        elevation: 5,
+        ...props.style
+    }}>{props.children}</View></TouchableOpacity>
+}
+
+export default Card;
