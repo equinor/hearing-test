f9f6e9d0c3ba8b39f08b6d16b27c6833bb7f46d4
Tormod Aase <toraas@equinor.com>
2021-06-25 15:55:48 +0200

    Functional component & optional props

--- app/components/common/EDS/Button.tsx
+++ app/components/common/EDS/Button.tsx
@@ -1,57 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { TouchableOpacity, View } from 'react-native';
 import Spinner from '../atoms/Spinner';
 import Typography from '../atoms/Typography';
 
-export default class ButtonEDS extends Component<{
-  onPress: () => void,
-  text: string,
-  outlined: boolean,
-  small: boolean,
-  danger: boolean,
-  loading: boolean,
-  disabled: boolean,
-}> {
-  getBackgroundColor(props) {
+const ButtonEDS = (props: {
+  onPress: () => void;
+  text: string;
+  outlined?: boolean;
+  danger?: boolean;
+  loading?: boolean;
+  disabled?: boolean;
+}) => {
+  function getBackgroundColor() {
     if (props.disabled || props.loading || props.outlined) return `#EAEAEA`;
     if (props.danger) return '#EB0000';
     return '#007079';
   }
 
-  render() {
-    const { danger, disabled, loading, onPress, outlined, small, text } = this.props;
-    return (
-      <TouchableOpacity onPress={onPress} disabled={loading || disabled}>
-        <View
-          style={{
-            height: 40,
-            width:160,
-            alignSelf: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: 4,
-            marginBottom: 18,
-            borderWidth: !danger && 1,
-            //width: small ? '50%' : '100%',
-            backgroundColor: this.getBackgroundColor(this.props),
-            borderColor: disabled || loading ? `#DCDCDC` : '#007079',
-          }}
-        >
-          {loading ? (
-            <Spinner color="#6F6F6F" />
-          ) : (
-            <Typography variant="button"
-              color={outlined ? '#007079' : 'white'}
-              style={{
-                textAlign: 'center',
-              }}
-            >
-              {text}
-            
-            </Typography>
-          )}
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
+  const { danger, disabled, loading, onPress, outlined, text } = props;
+  return (
+    <TouchableOpacity onPress={onPress} disabled={loading || disabled}>
+      <View
+        style={{
+          height: 40,
+          width: 160,
+          alignSelf: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: 4,
+          marginBottom: 18,
+          borderWidth: !danger && 1,
+          backgroundColor: getBackgroundColor(),
+          borderColor: disabled || loading ? `#DCDCDC` : '#007079',
+        }}
+      >
+        {loading ? (
+          <Spinner color="#6F6F6F" />
+        ) : (
+          <Typography
+            variant="button"
+            color={outlined ? '#007079' : 'white'}
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            {text}
+          </Typography>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+ButtonEDS.defaultProps = {
+  outlined: false,
+  danger: false,
+  loading: false,
+  disabled: false,
+};
+
+export default ButtonEDS;
