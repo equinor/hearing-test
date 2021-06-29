5926b61a1d8b93e731baefdc061f67c446766450
Tormod Aase <toraas@equinor.com>
2021-06-29 11:31:30 +0200

    Merge conflict

--- app/components/common/atoms/BigRoundButton.tsx
+++ app/components/common/atoms/BigRoundButton.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const BigRoundButton = (props: { onPress: () => void; text: string; disabled: boolean }) => {
+const BigRoundButton = (props: {
+  onPress: () => void;
+  text: string;
+  disabled?: boolean;
+  variant?: 'primary' | 'secondary';
+}) => {
   const diameter = 241;
   const { disabled, text, onPress } = props;
   return (
@@ -15,12 +20,17 @@ const BigRoundButton = (props: { onPress: () => void; text: string; disabled: bo
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: diameter / 2,
-        backgroundColor: disabled ? '#F5F5F5' : '#007079',
+        backgroundColor: disabled || props.variant === 'secondary' ? '#F5F5F5' : '#007079',
       }}
     >
       <Text style={{ color: disabled ? '#666666' : 'white', fontSize: 17 }}>{text}</Text>
     </TouchableOpacity>
   );
+};
+
+BigRoundButton.defaultProps = {
+  disabled: false,
+  variant: 'primary',
 };
 
 export default BigRoundButton;
