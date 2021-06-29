1b5c905fbf9b30613ef12b7baf957aafc18efc35
Tormod Aase <toraas@equinor.com>
2021-06-29 17:45:39 +0200

    Secondary version of button added

--- app/components/common/atoms/BigRoundButton.tsx
+++ app/components/common/atoms/BigRoundButton.tsx
@@ -9,6 +9,16 @@ const BigRoundButton = (props: {
 }) => {
   const diameter = 241;
   const { disabled, text, onPress } = props;
+
+  let borderWidth = 0;
+  const borderColor = '#007079';
+  let textColor = 'white';
+  let backgroundColor = '#007079';
+  if (props.variant === 'secondary') {
+    borderWidth = 1;
+    textColor = '#007079';
+    backgroundColor = 'transparent';
+  }
   return (
     <TouchableOpacity
       testID="BigRoundButton"
@@ -19,11 +29,14 @@ const BigRoundButton = (props: {
         width: diameter,
         alignItems: 'center',
         justifyContent: 'center',
+        borderColor,
+        borderWidth,
+
         borderRadius: diameter / 2,
-        backgroundColor: disabled || props.variant === 'secondary' ? '#F5F5F5' : '#007079',
+        backgroundColor: disabled || props.variant === 'secondary' ? '#EFEFEF' : backgroundColor,
       }}
     >
-      <Text style={{ color: disabled ? '#666666' : 'white', fontSize: 17 }}>{text}</Text>
+      <Text style={{ color: disabled ? '#666666' : textColor, fontSize: 17 }}>{text}</Text>
     </TouchableOpacity>
   );
 };
