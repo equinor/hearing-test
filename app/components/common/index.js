25f784ad68fafb4c86a117596477079ccd410d52
Dan André Svendsen <dsven@equinor.com>
2022-05-21 13:01:03 +0200

    Delete component - NavigationList

--- app/components/common/index.js
+++ app/components/common/index.js
@@ -1,6 +1,4 @@
-export { default as Button } from './atoms/Button';
-export { default as Checkbox } from './atoms/Checkbox';
-export { default as Spinner } from './atoms/Spinner';
-export { default as TextLink } from './atoms/TextLink';
-export { default as SimpleInfoItem } from './atoms/SimpleInfoItem';
-export { default as NavigationList } from './molecules/NavigationList';
+export { default as Button } from "./atoms/Button";
+export { default as Spinner } from "./atoms/Spinner";
+export { default as TextLink } from "./atoms/TextLink";
+export { default as SimpleInfoItem } from "./atoms/SimpleInfoItem";
