81393adbfdb5964d8429dffbf9964f8adbd010d9
Dan André Svendsen <dsven@equinor.com>
2022-05-21 16:19:54 +0200

    Delete component - AppContainer

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 
-import AppContainer from "./components/common/atoms/AppContainer";
 import useCachedResources from "./hooks/useCachedResources";
 import Navigation from "./navigation";
 import store from "./store/config";
@@ -14,14 +13,12 @@ export default function App() {
     return null;
   } else {
     return (
-      <AppContainer>
-        <Provider store={store}>
-          <SafeAreaProvider>
-            <Navigation colorScheme="light" />
-            <StatusBar />
-          </SafeAreaProvider>
-        </Provider>
-      </AppContainer>
+      <Provider store={store}>
+        <SafeAreaProvider>
+          <Navigation colorScheme="light" />
+          <StatusBar style="dark" />
+        </SafeAreaProvider>
+      </Provider>
     );
   }
 }
