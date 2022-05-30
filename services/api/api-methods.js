810ad482e80e52b01876f71e249c6baa8b5542a9
Dan André Svendsen <dsven@equinor.com>
2022-05-30 13:16:15 +0200

    Set headset profiles to -55 & keep audio resources

--- services/api/api-methods.js
+++ services/api/api-methods.js
@@ -83,9 +83,9 @@ export const postTakeTest = () =>
     hz500Db: 0,
     hz1000Db: 0,
     hz2000Db: 0,
-    hz3000Db: -40,
-    hz4000Db: -50,
-    hz6000Db: -60,
+    hz3000Db: -55,
+    hz4000Db: -55,
+    hz6000Db: -55,
     hz8000Db: 0,
   });
 
