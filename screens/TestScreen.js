aba4229c69637a66723b4b644c834b90bbe8a71c
PtjeEquinor <ptje@equinor.com>
2022-06-01 13:29:30 +0200

    Add criteria to test run logic

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -98,7 +98,11 @@ class TestScreen extends Component {
       !this.props.testIsFinished &&
       (this.props.node !== prevProps.node || this.state.nextNodeWaiting)
     ) {
-      if (!this.state.pauseAfterNode && !this.state.modalVisible) {
+      if (
+        !this.state.pauseAfterNode &&
+        !this.state.modalVisible &&
+        this.props.testIsRunning
+      ) {
         this.runNode(this.props.node);
         if (this.state.nextNodeWaiting)
           this.setState({ nextNodeWaiting: false }); // eslint-disable-line react/no-did-update-set-state
