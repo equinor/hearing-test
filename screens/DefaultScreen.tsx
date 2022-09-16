18295b6d3a2f18ce0caaa8f683f8909a7cd7bc48
Adrian Nesvik <adrian.nesvik@outlook.com>
2022-09-16 12:53:01 +0200

    Fix location help text

--- screens/DefaultScreen.tsx
+++ screens/DefaultScreen.tsx
@@ -168,7 +168,7 @@ class DefaultScreen extends Component<{
           setInvisible={() => this.setLocationHelpModalVisible(false)}
         >
           <Typography variant="p" style={{ paddingTop: 32, paddingHorizontal: 24 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Du kan oppdatere lokasjonen din i SAP dersom den er ukorrekt.
           </Typography>
         </SlideModal>
       </ScrollView>
