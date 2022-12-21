512b00ae29ee172fd1d24143b93add130abd9b84
Dan André Svendsen <dsven@equinor.com>
2022-12-21 14:14:37 +0100

    refactor: Indicators

--- components/common/molecules/Indicators.tsx
+++ components/common/molecules/Indicators.tsx
@@ -1,58 +1,45 @@
 import React from "react";
-import { StyleProp, View, ViewStyle } from "react-native";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
 
-import { EQUINOR_GREEN } from "../../../constants/colors";
+import { MOSS_GREEN_100 } from "../../../constants/colors";
 
 type Props = {
   iterable: any[];
-  style: StyleProp<ViewStyle>;
+  style?: StyleProp<ViewStyle>;
 };
 
-export const Indicators: React.FC<Props> = ({ iterable = [], style = {} }) => {
-  return (
-    <View
-      style={[
-        {
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-        },
-        style,
-      ]}
-    >
-      {iterable.map(({ current, hideIndicator }, index) => {
-        if (hideIndicator) {
-          return null;
-        }
+export const Indicators: React.FC<Props> = ({ iterable = [], style }) => (
+  <View style={[styles.container, style]}>
+    {iterable.map(({ current, hideIndicator }, index) => {
+      if (hideIndicator) {
+        return null;
+      }
 
-        if (current) {
-          return (
-            <View
-              key={index}
-              style={{
-                borderRadius: 16,
-                width: 20,
-                height: 10,
-                backgroundColor: EQUINOR_GREEN,
-                margin: 2,
-              }}
-            />
-          );
-        }
+      return (
+        <View
+          key={index}
+          style={[
+            styles.indicator,
+            {
+              width: current ? 20 : 12,
+              height: current ? 10 : 8,
+              backgroundColor: current ? MOSS_GREEN_100 : "#97CACE",
+            },
+          ]}
+        />
+      );
+    })}
+  </View>
+);
 
-        return (
-          <View
-            key={index}
-            style={{
-              borderRadius: 16,
-              width: 12,
-              height: 8,
-              backgroundColor: "#97CACE",
-              margin: 2,
-            }}
-          />
-        );
-      })}
-    </View>
-  );
-};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  indicator: {
+    borderRadius: 16,
+    margin: 2,
+  },
+});
