969c354250596c7b0c5a1a683c9c35a9bdbbd01c
Dan André Svendsen <dsven@equinor.com>
2023-02-17 15:04:42 +0100

    feat: Add labels for test result graph
    This makes it easier for the user to understand which ear the data in the test result graph belongs to.
    
    Closes #174

---
+++ components/common/atoms/EarLabel.tsx
@@ -0,0 +1,30 @@
+import { Typography } from "mad-expo-core";
+import React from "react";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+
+import { LEFT_EAR, RIGHT_EAR } from "../../../constants/colors";
+import { Ear } from "../../../types";
+
+type Props = { ear: Ear; style?: StyleProp<ViewStyle> };
+
+export const EarLabel: React.FC<Props> = ({ ear, style }) => (
+  <View style={[styles.container, style]}>
+    <View
+      style={[
+        styles.circle,
+        { backgroundColor: ear === "left" ? LEFT_EAR : RIGHT_EAR },
+      ]}
+    />
+    <Typography>{ear === "left" ? "Venstre" : "Høyre"} øre</Typography>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flexDirection: "row" },
+  circle: {
+    height: 16,
+    width: 16,
+    borderRadius: 8,
+    marginRight: 8,
+  },
+});
