2c4cae9d69d35907eb5ebf2ebe85e046ef771285
Øyvind Fredstie <oyvind.fredstie@bouvet.no>
2023-05-03 13:26:13 +0200

    Changed to implicit return

--- utils/date.ts
+++ utils/date.ts
@@ -1,11 +1,10 @@
 import { ISOString } from "../types";
 
-export const formatDate = (date: ISOString) => {
-  return new Date(date).toLocaleDateString("nb-NO", {
+export const formatDate = (date: ISOString) =>
+  new Date(date).toLocaleDateString("nb-NO", {
     day: "2-digit",
     month: "short",
     year: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
   });
-};
