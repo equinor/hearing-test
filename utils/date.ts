3c36d6aafee4292b1023ffca1c022add6dba60c9
Øyvind Fredstie <oyvind.fredstie@bouvet.no>
2023-05-03 13:15:36 +0200

    Minor adjustment to order of date options

--- utils/date.ts
+++ utils/date.ts
@@ -2,8 +2,8 @@ import { ISOString } from "../types";
 
 export const formatDate = (date: ISOString) => {
   return new Date(date).toLocaleDateString("nb-NO", {
-    month: "short",
     day: "2-digit",
+    month: "short",
     year: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
