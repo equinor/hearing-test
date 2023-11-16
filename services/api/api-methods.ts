5bf871fbb81fdb8a9b0449c4ed326aefdcaca032
Dan André Svendsen <dsven@equinor.com>
2023-11-16 20:56:23 +0100

    fix: Headset calibration factors

--- services/api/api-methods.ts
+++ services/api/api-methods.ts
@@ -86,13 +86,13 @@ export const postTakeTest = () =>
   store.getState().appConfig.isDemoMode
     ? postMockTakeTest()
     : postData(`/me/tests/takeTest`, {
-        hz500Db: -71.4,
-        hz1000Db: -71.4,
+        hz500Db: -74.2,
+        hz1000Db: -74.4,
         hz2000Db: -71.4,
         hz3000Db: -71.4,
         hz4000Db: -65.9,
         hz6000Db: -59.6,
-        hz8000Db: -71.4,
+        hz8000Db: -60.9,
       });
 
 export const postTest = (body) => postData(`/me/tests`, body);
