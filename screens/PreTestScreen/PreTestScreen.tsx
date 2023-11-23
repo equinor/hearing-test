55e051467e3f8583cf986416d55d75ede2097113
Dan André Svendsen <dsven@equinor.com>
2023-11-23 09:42:52 +0100

    refactor: Use `Typography` from `@equinor/mad-components`

--- screens/PreTestScreen/PreTestScreen.tsx
+++ screens/PreTestScreen/PreTestScreen.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@equinor/mad-components";
-import { Typography } from "mad-expo-core";
+import { Button, Typography } from "@equinor/mad-components";
 import { Image, ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { usePreTestPages } from "./hooks/usePreTestPages";
 import { ButtonGroup } from "../../components/common/atoms/ButtonGroup";
 import { Indicators } from "../../components/common/molecules/Indicators";
-import { MOSS_GREEN_100, TEXT } from "../../constants/colors";
 import { RootStackScreenProps } from "../../types";
 import { confirmationDialog } from "../../utils/alerts";
 import { BarCodeScannerScreen } from "../BarCodeScannerScreen";
@@ -42,21 +40,14 @@ export const PreTestScreen = ({ navigation }: PreTestScreenProps) => {
             variant="ghost"
           />
         </View>
-        <Typography variant="h1" color={MOSS_GREEN_100} style={styles.title}>
+        <Typography variant="h2" color="primary" style={styles.title}>
           {page.title}
         </Typography>
         <Image source={page.image} style={styles.image} />
         {page.hideIndicators ? null : (
           <Indicators iterable={pages} style={styles.indicators} />
         )}
-        <Typography
-          numberOfLines={4}
-          size={18}
-          color={TEXT}
-          style={styles.description}
-        >
-          {page.description}
-        </Typography>
+        <Typography style={styles.description}>{page.description}</Typography>
         <ButtonGroup>
           {page.buttons.map((props) => (
             <Button key={props.title} {...props} style={styles.button} />
@@ -69,6 +60,7 @@ export const PreTestScreen = ({ navigation }: PreTestScreenProps) => {
 
 const styles = StyleSheet.create({
   contentContainer: {
+    flex: 1,
     padding: 24,
     alignItems: "center",
   },
@@ -80,7 +72,5 @@ const styles = StyleSheet.create({
   description: {
     textAlign: "center",
     marginBottom: 32,
-    height: 24 * 4,
-    lineHeight: 24,
   },
 });
