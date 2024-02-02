1c7de11c3296a79a11911b7bc6c74eff8781b516
Dan André Svendsen <dsven@equinor.com>
2024-02-02 15:19:30 +0100

    refactor: Remove duplicate `useInitialSystemVolume`
    Closes #348

--- hooks/useInitialSystemVolume.ts
+++ hooks/useInitialSystemVolume.ts
@@ -1,13 +1,26 @@
+import { trackCustom } from "@equinor/mad-core";
 import { useEffect, useState } from "react";
 import SystemSetting from "react-native-system-setting";
 
 import { SYSTEM_VOLUME } from "../constants/sounds";
+import { setSystemVolume } from "../utils/volume/setSystemVolume";
 
 export const useInitialSystemVolume = () => {
   const [initialSystemVolume, setInitialSystemVolume] = useState(SYSTEM_VOLUME);
 
   useEffect(() => {
-    SystemSetting.getVolume().then((volume) => setInitialSystemVolume(volume));
+    const setInitialDeviceSystemVolume = async () => {
+      await SystemSetting.getVolume()
+        .then((volume) => setInitialSystemVolume(volume))
+        .catch((error) => {
+          trackCustom("useInitialSystemVolume: Error getting system volume", {
+            error,
+          });
+        });
+    };
+    setInitialDeviceSystemVolume();
+
+    return () => setSystemVolume(initialSystemVolume);
   }, []);
 
   return { initialSystemVolume };
